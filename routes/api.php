1b00cddff08b302b1d5e88cb6d040755ab10bfcb
nghiachauphi <72390567+nghiachauphi@users.noreply.github.com>
2022-06-08 23:48:33 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+ Route::get('/user/all', function (Request $request) {
+        return \App\Models\User::all();
+    });
 Route::post('/login', [App\Http\Controllers\UserController::class, 'login']);
 Route::post('/logout', [App\Http\Controllers\UserController::class, 'logout']);
 Route::post('/user/create', [App\Http\Controllers\UserController::class, 'create']);
@@ -21,9 +23,7 @@ Route::post('/user/delete/', [App\Http\Controllers\UserController::class, 'delet
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/user', [App\Http\Controllers\UserController::class, 'index'])->name('user');
-    Route::get('/user/all', function (Request $request) {
-        return \App\Models\User::all();
-    });
+   
     Route::post('/change_password', [App\Http\Controllers\UserController::class, 'change_password']);
     Route::post('/user/update', [App\Http\Controllers\UserController::class, 'update']);
 
