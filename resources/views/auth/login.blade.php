baae4c2e80cc26162a3731fb08ed1b7a33a8c1c8
Huynh Trong Nghia <trongnghia.cl.tb.ag@gmail.com>
2022-06-13 14:52:28 +0700

    fix layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('postlogin') }}">
+                    <form method="POST" >{{--action="{{ route('postlogin') }}"--}}
                         @csrf
 
                         <div class="row mb-3">
